1aecbc58efb4d28da757e8f25eab91e83147e6bd
Omar <omar-aljalam@outlook.com>
2026-01-11 03:46:53 +0200

    Player logic! Changing Names using two-ways binding

---
+++ src/components/Player.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // React is scheduling State updates, so we use the functional form
+    // Scheduling state upadtes means that the change is not immediate
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let buttonText = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleNameChange}
+      />
+    );
+    buttonText = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{buttonText}</button>
+    </li>
+  );
+}
